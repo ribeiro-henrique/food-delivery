8b819129d16ba8f1aa9c0191a4528f98eaf43725
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-11-18 10:33:17 -0300

    vue project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <NavBar />
+  <router-view/>
+</template>
+
+<script>
+
+import NavBar from './components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="nav">
+    <router-link to="/">
+      <img src="" id="logo">
+    </router-link>
+  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "NavBar"
+  }
+
+</script>
+
+<style scoped>
+
+</style>
